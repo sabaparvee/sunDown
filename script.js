865b0a72f27b94bb8abccd10c131ad554727357f
saba parveen <psaba2040@gmail>
2024-07-07 19:59:53 +0530

    finalcommit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-/* const scroll = new LocomotiveScroll({
+const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
-}); */
+});
 function pageAnimation(){
 var elemC=document.querySelector('#elem-container');
 var fixed=document.querySelector('#fixed-image')
@@ -30,22 +30,33 @@ function swiperAnimation() {
         },
       });}
 
+function menuAnimation(){
+    var menu=document.querySelector('nav h3')
+    var full =document.querySelector('#ful-scr')
+    var navimage=document.querySelector('nav img')
+    var flag=0
+    menu.addEventListener('click',function(){
+      if(flag==0){
+          full.style.top=0
+          navimage.style.opacity=0
+          flag=1
+      }
+      else{
+          full.style.top="-100%"
+          navimage.style.opacity=1
+          flag=0
+      }
+    }) 
+}
 
-  var menu=document.querySelector('nav h3')
-  var full =document.querySelector('#ful-scr')
-  var navimage=document.querySelector('nav img')
-  var flag=0
-  menu.addEventListener('click',function(){
-    if(flag==0){
-        full.style.top=0
-        navimage.style.opacity=0
-        flag=1
-    }
-    else{
-        full.style.top="-100%"
-        navimage.style.opacity=1
-        flag=0
-    }
-  })    
+function loader(){
+    var loader=document.querySelector('#loader')
+setTimeout(function(){
+    loader.style.top='-100%'
+},4200)
+}
+
+loader()
+menuAnimation()    
 swiperAnimation()
 pageAnimation()
